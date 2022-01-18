b3fe3ab030f5d1ea5f465c5714bbdeb33d41f773
nictamhk <sjspfirst@yahoo.com.hk>
2022-01-18 17:55:42 +0800

    Subtotal action added

--- actions/actions.py
+++ actions/actions.py
@@ -7,6 +7,7 @@ from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from rasa_sdk.events import SlotSet
+#from rasa_sdk.forms import FormAction
 
 ### GLOBAL VARIABLE - Menu of the restaurant
 MENU = {
@@ -45,27 +46,27 @@ class ValidateOrderForm (FormValidationAction):
         
         # Check if item is offered
         if (user_main is not None and user_main not in MENU['main']):
-            dispatcher.utter_message(template = "utter_item_not_offered")
+            dispatcher.utter_message(response = "utter_item_not_offered")
             return {"user_main": None}
         
         elif (user_side is not None and user_side not in MENU['side']):
-            dispatcher.utter_message(template = "utter_item_not_offered")
+            dispatcher.utter_message(response = "utter_item_not_offered")
             return {"user_side": None}
         
         elif (user_drink is not None and user_drink not in MENU['drink']):
-            dispatcher.utter_message(template = "utter_item_not_offered")
+            dispatcher.utter_message(response = "utter_item_not_offered")
             return {"user_drink": None}
         
         elif (user_main is not None and user_main in MENU['main']):
-            dispatcher.utter_message("Alright,", user_main, "for the main course.")
+            dispatcher.utter_message("Alright, so", str(user_main), "for the main course.")
             return {"user_main": user_main} #Validated
         
         elif (user_side is not None and user_side in MENU['side']):
-            dispatcher.utter_message("Got it,", user_side, "for your side.")
+            dispatcher.utter_message("Got it,", str(user_side), "for your side.")
             return {"user_side": user_side} #Validated
         
         elif (user_drink is not None and user_drink in MENU['drink']):
-            dispatcher.utter_message("Okay,", user_drink, "to drink.")
+            dispatcher.utter_message("Okay,", str(user_drink), "to drink.")
             return {"user_drink": user_drink} #Validated
         
                 
@@ -118,7 +119,7 @@ class CalculateSubTotal (Action):
             return {"user_main": slot_value}
     
         else:
-            dispatcher.utter_message(template = "utter_item_not_offered")
+            dispatcher.utter_message(response = "utter_item_not_offered")
             return {"user_main": None}
         
     def validate_user_side(self, slot_value, dispatcher, tracker, domain):
@@ -139,6 +140,9 @@ class CalculateSubTotal (Action):
         side_ordered = tracker.get_slot("user_side")
         drink_ordered = tracker.get_slot("user_drink")
         
+        # Option when customer wants to confirm order
+        confirm_order = tracker.get_slot("confirm_order")
+        
         # Track if the user has finished ordering
         end_order = tracker.get_slot("end_order")
         
@@ -150,10 +154,18 @@ class CalculateSubTotal (Action):
         
         if slot_value.lower() in self.menu_db("main"):
             price += price_list[slot_value.lower()]
+            SlotSet("subtotal", price)
             
         else:
-            dispatcher.utter_message(template = "utter_item_not_offered")
+            dispatcher.utter_message(response = "utter_item_not_offered")
             price = 0
+            
+        if confirm_order:
+            dispatcher.utter_message("So this is what you've ordered:", \
+                                     str(main_ordered or ''), ", ", \
+                                     str(side_ordered or ''), ", ", \
+                                     str(drink_ordered or ''),
+                                     ". Does that sound right?")
         
         if end_order:
-            dispatcher.utter_message(template = "utter_total", subtotal = str(curr_subtotal))
+            dispatcher.utter_message(response = "utter_total", subtotal = str(curr_subtotal))
